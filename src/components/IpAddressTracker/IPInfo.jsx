ae1823dcb33b0fe222e3159c7aaaca6d969ba804
kfulkerson-pcg <kfulkerson@phxcapitalgroup.com>
2022-10-27 18:19:02 -0700

    small change

--- src/components/IpAddressTracker/IPInfo.jsx
+++ src/components/IpAddressTracker/IPInfo.jsx
@@ -46,22 +46,22 @@ const IPInfo = () => {
       <div className="hidden lg:flex max-w-[1110px] w-[80%] h-[161px] mx-auto bg-white rounded-lg shadow-lg p-8 absolute bottom-[-80px] justify-between">
         <div className="w-full">
           <p className="text-gray-400 text-[12px] font-[700] uppercase tracking-[.15em] mb-2">ip address</p>
-          <p className="text-[26px] font-[500]">{ipAddress}</p>
+          <p className="text-[20px] xl:text-[26px] font-[500]">{ipAddress}</p>
         </div>
         <div className="h-[75px] border-l border-gray-200 mt-3 mr-8" />
         <div className="w-full">
           <p className="text-gray-400 text-[12px] font-[700] uppercase tracking-[.15em] mb-2">location</p>
-          <p className="text-[26px] font-[500]">{location}</p>
+          <p className="text-[20px] xl:text-[26px] font-[500]">{location}</p>
         </div>
         <div className="h-[75px] border-l border-gray-200 mt-3 mr-8" />
         <div className="w-full">
           <p className="text-gray-400 text-[12px] font-[700] uppercase tracking-[.15em] mb-2">timezone</p>
-          <p className="text-[26px] font-[500]">{timezone}</p>
+          <p className="text-[20px] xl:text-[26px] font-[500]">{timezone}</p>
         </div>
         <div className="h-[75px] border-l border-gray-200 mt-3 mr-8" />
         <div className="w-full">
           <p className="text-gray-400 text-[12px] font-[700] uppercase tracking-[.15em] mb-2">isp</p>
-          <p className="text-[26px] font-[500] w-24">{isp}</p>
+          <p className="text-[20px] xl:text-[26px] font-[500] w-24">{isp}</p>
         </div>
       </div>
     </>
